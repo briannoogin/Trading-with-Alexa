97c81d53d95a873eca81c892657bd7dd010bea0b
madhavmehta1 <madhavm16@gmail.com>
2018-11-01 16:16:59 -0500

    added current price to quote function

--- lambda/py/alexa/util.py
+++ lambda/py/alexa/util.py
@@ -14,12 +14,13 @@ def get_stock_quote(stock_symbol):
     # type: (String) -> json
     quote_data = requests.get(BASE_URL + stock_symbol + QUOTE_URL).json()
     return_quote_data = {
-        'symbol' : quote_data["symbol"],
-        'name' : quote_data["companyName"],
-        'sector' : quote_data["sector"],
-        'primary exchange' : quote_data["primaryExchange"],
-        'open price' : quote_data["open"],
-        'change percentage' : quote_data["changePercent"]        
+        'symbol' : quote_data['symbol'],
+        'name' : quote_data['companyName'],
+        'sector' : quote_data['sector'],
+        'primary_exchange' : quote_data['primaryExchange'],
+        'open_price' : quote_data['open'],
+        'current_price' : quote_data['calculationPrice'],
+        'change_percentage' : quote_data['changePercent']        
     }
     return json.dumps(return_quote_data)
 
@@ -28,10 +29,10 @@ def get_stock_news(stock_symbol):
     # type: (String) -> json
     news_data = requests.get(BASE_URL + stock_symbol + NEWS_URL).json()
     return_news_data = {
-        'date' : news_data["datetime"],
-        'title' : news_data["headline"],
-        'source' : news_data["source"],
-        'summary' : news_data["summary"] 
+        'date' : news_data['datetime'],
+        'title' : news_data['headline'],
+        'source' : news_data['source'],
+        'summary' : news_data['summary'] 
     }
     return json.dumps(return_news_data)
 
@@ -40,18 +41,18 @@ def get_stock_keystats(stock_symbol):
     # type: (String) -> json
     keystats_data = requests.get(BASE_URL + stock_symbol + KEYSTATS_URL).json()
     return_keystats_data = {
-        'name': keystats_data["companyName"],
-        'yearHigh' : keystats_data["week52high"],
-        'yearLow' : keystats_data["week52low"],
-        'yearChange' : keystats_data["week52change"],
-        'latestEPS' : keystats_data["latestEPS"],
-        'latestEPSDate' : keystats_data["latestEPSDate"],
-        'peRatioHigh' : keystats_data["peRatioHigh"],
-        'peRatioLow' : keystats_data["peRatioLow"],
-        'priceToSale' : keystats_data["priceToSales"],
-        'priceToBook' : keystats_data["priceToBook"],
-        'day200MovingAvg' : keystats_data["day200MovingAvg"],
-        'day50MovingAvg' : keystats_data["day50MovingAvg"]
+        'name': keystats_data['companyName'],
+        'yearHigh' : keystats_data['week52high'],
+        'yearLow' : keystats_data['week52low'],
+        'yearChange' : keystats_data['week52change'],
+        'latestEPS' : keystats_data['latestEPS'],
+        'latestEPSDate' : keystats_data['latestEPSDate'],
+        'peRatioHigh' : keystats_data['peRatioHigh'],
+        'peRatioLow' : keystats_data['peRatioLow'],
+        'priceToSale' : keystats_data['priceToSales'],
+        'priceToBook' : keystats_data['priceToBook'],
+        'day200MovingAvg' : keystats_data['day200MovingAvg'],
+        'day50MovingAvg' : keystats_data['day50MovingAvg']
     }
     return json.dumps(return_keystats_data)
 
@@ -60,9 +61,14 @@ def get_stock_price(stock_symbol):
     # type: (String) -> json
     price_data = requests.get(BASE_URL + stock_symbol + PRICE_URL)
     return_price_data = {
-        'price' : price_data["price"]
+        'price' : price_data['price']
     }
     return json.dumps(return_price_data)
+
+# (INCORRECT, FIX!!) Return the stock ticker symbol of a company
+def get_stock_symbol(company_Name):
+    symbol = company_Name
+    return symbol
 
 # Resolve the slot name from the request
 def get_resolved_value(request, slot_name):
