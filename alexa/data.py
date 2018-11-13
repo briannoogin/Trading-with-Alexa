0de7958699a2920880ac6865747ca09847c3f127
Cameron O'Brien <cjo170330@utdallas.edu>
2018-11-13 11:14:09 -0600

    Merge complete feature/lambda branch with master, now with stock symbol conversion

---
+++ alexa/data.py
@@ -0,0 +1,58 @@
+# Standard Alexa Messages
+SKILL_NAME = "Stock Informing"
+WELCOME_MESSAGE = (
+    "Welcome to Stock Informing with Alexa! "
+    "This skill helps inform the user of the current state of a company's stock."
+    )
+
+QUOTE_MESSAGE = (
+    "Here's your quote: "
+    "The stock symbol is {}. "
+    "The company name is {}. "
+    "It is in the {} sector. "
+    "The primary exchange is {}. "
+    "Opening price was ${}. "
+    "The current price is ${}. "
+    "The percent change from previous closing price is {} percent."
+    )
+NEWS_MESSAGE = (
+    "Here's the news: "
+    "The headline is: {}. "
+    "Sourced by {}. "
+    )
+KEYSTATS_MESSAGE = (
+    "Here are the key stats: "
+    "The company name is {}. "
+    "The year highs and lows are ${} and ${} respectively. "
+    "The yearly change in price is {}%. "
+    "Latest earnings per share is ${} as of {}. "
+    "The price to earnings ratio highs and lows are {} and {} respectively. "
+    "The price to book ratio is {} and the price to sales ratio is {}. "
+    "The day 200 moving average is ${}. "
+    "The day 50 moving average is ${}. "
+    )
+TRENDSTATS_MESSAGE = (
+    "Here are the trend stats: "
+    "The company name is {}. "
+    "Year highs and lows are ${} and ${} respectively. "
+    "The yearly change in price is {} percent. "
+    "Earnings before interest, tax, depreciation, and amortization is ${}. "
+    "The day 200 moving average is ${}. "
+    "The day 50 moving average is ${}. "
+    "Year 5 change is {} percent. "
+    "Year 2 change is {} percent. "
+    "Year 1 change is {} percent. "
+    "Month 6 change is {} percent. "
+    "Month 1 change is {} percent. "
+    "Day 5 change is {} percent."
+    )
+
+PRICE_MESSAGE = (
+    "The current stock price for {} is ${}."
+    )
+HELP_MESSAGE = "You can ask me to give you a quote, news, key stats, and/or price on a company's stock. Or you can say exit."
+HELP_PROMPT = "What can I help you with?"
+STOP_MESSAGE = "Goodbye!"
+FALLBACK_MESSAGE = "The Stock Informing skill can't help with that. It can inform you on varying levels of information on a company's stock"
+FALLBACK_PROMPT = "What can I help you with?"
+EXCEPTION_MESSAGE = "Sorry. I cannot help you with that."
---
+++ alexa/util.py
@@ -0,0 +1,54 @@
+import requests
+import json
+from ask_sdk_model import IntentRequest
+
+# URLs for API requests
+BASE_URL = "https://api.iextrading.com/1.0/stock/"
+QUOTE_URL = "/quote?displayPercent=true"
+NEWS_URL = "/news"
+KEYSTATS_URL = "/stats"
+PRICE_URL = "/price"
+
+
+# Return a quote  for a company's stock
+def get_stock_quote(stock_symbol):
+    # type: (String) -> json
+    return requests.get(BASE_URL + stock_symbol + QUOTE_URL).json()
+
+
+# Return news regarding a company's stock
+def get_stock_news(stock_symbol):
+    # type: (String) -> json
+    return requests.get(BASE_URL + stock_symbol + NEWS_URL).json()[0]
+
+# Return long term stats of a company's stock
+def get_stock_trendstats(stock_symbol):
+    # type: (String) -> json
+    return requests.get(BASE_URL + stock_symbol + KEYSTATS_URL).json()
+
+
+# Return key stats of a company's stock
+def get_stock_keystats(stock_symbol):
+    # type: (String) -> json
+    return requests.get(BASE_URL + stock_symbol + KEYSTATS_URL).json()
+
+
+# Return price data of a company's stock
+def get_stock_price(stock_symbol):
+    # type: (String) -> float
+    return requests.get(BASE_URL + stock_symbol + PRICE_URL).json()
+
+
+# (INCORRECT, FIX!!) Return the stock ticker symbol of a company
+def get_stock_symbol(company_name):
+    request_url = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en'.format(company_name)
+    return requests.get(request_url).json()
+
+
+# Resolve the slot name from the request
+def get_resolved_value(request, slot_name):
+    # type: (IntentRequest, str) -> Union[str, None]
+    try:
+        return request.intent.slots[slot_name].value
+    except (AttributeError, ValueError, KeyError, IndexError):
+        return None
